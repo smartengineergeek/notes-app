268a63ef1a1a91ac6088825655d74cd590557bf5
amahlawat <avinash532110@gmail.com>
2020-11-15 17:45:34 +0530

    drag and drop implemented success

--- src/components/Notes/Note/Note.js
+++ src/components/Notes/Note/Note.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './Note.css';
 
 const Note = props => (
-    <div className="single-note">
-        <p className="single-note-heading">{props.heading}</p>
-        <p className="single-note-description">
-        {props.description}
-        </p>
-        <button className="single-note-view-button" onClick={props.clickHandler}>View</button>
-    </div>
+        <div className="single-note" id={props.note.id} >
+            <p className="single-note-heading">{props.note.heading}</p>
+            <p className="single-note-description">{props.note.description}</p>
+            <button className="single-note-view-button" onClick={props.note.clickHandler}>View</button>
+        </div>
 )
 
 export default Note;
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -7,17 +7,84 @@ class Notes extends React.Component{
     clickHandler = () => {
         document.getElementById("myModal").style.display = "block";
     }
+    onDragStart(event) {
+        event.dataTransfer.effectAllowed = "move";
+        console.log(" drag start ", event.target.getAttribute('id'))
+        event.dataTransfer.setData("text", event.target.getAttribute('id'));
+        // event.currentTarget.style.backgroundColor = "#e9c46a";
+    }  
+    onDragEnd(event) {
+        // event.currentTarget.style.backgroundColor = "#2a9d8f";
+    }      
+    onDragOver(event) {
+        event.preventDefault();
+    }      
+    onDrop = (event) => {
+        if((event.target.className).includes('drop-from-selected')){
+            // selected
+            let dropzone = document.getElementById("dropzone-inprogress");
+      
+            let id = event.dataTransfer.getData("text");
+            console.log(" event class ", event.target.className, " id ", id);
+            
+            if(id != ""){
+                const draggableElement = document.getElementById(id);
+                const clone = draggableElement.cloneNode(true);
+            
+                // dropzone.appendChild(clone);
+                if(localStorage.getItem("tasks-app")){
+                    let tasks = JSON.parse(localStorage.getItem("tasks-app"));
+                    for(let task of tasks){
+                        if((task.id).toString() === id.split("-")[1])
+                            task.status = "inprogress";                     
+                    }
+                    localStorage.setItem("tasks-app", JSON.stringify(tasks));
+                }
+                if(document.getElementById(id.split("-")[1]) != undefined)
+                    document.getElementById(id.split("-")[1]).remove();
+            }
+        }else if((event.target.className).includes('drop-from-inprogress')){
+            // inprogress
+            let dropzone = document.getElementById("dropzone-done");
+      
+            let id = event.dataTransfer.getData("text");
+            console.log(" event class ", event.target.className, " id ", id);
+    
+            if(id != ""){
+                const draggableElement = document.getElementById(id);
+                const clone = draggableElement.cloneNode(true);
+            
+                // dropzone.appendChild(clone);
+                if(localStorage.getItem("tasks-app")){
+                    let tasks = JSON.parse(localStorage.getItem("tasks-app"));
+                    for(let task of tasks){
+                        if((task.id).toString() === id.split("-")[1])
+                            task.status = "done";                     
+                    }
+                    localStorage.setItem("tasks-app", JSON.stringify(tasks));
+                }
+                if(document.getElementById(id.split("-")[1]) != undefined)
+                    document.getElementById(id.split("-")[1]).remove();
+            }
+        }
+      
+       event.dataTransfer.clearData();        
+       this.forceUpdate();
+    }
+
     render(){
         let notes = JSON.parse(localStorage.getItem("tasks-app"));
         return(
             <div className="tasks">
-                <div className="tasks-selected-for-development tasks-col">
-                    <p className="tasks-type">Selected</p>
+                <div className="tasks-selected tasks-col">
+                    <p className="tasks-type">Selected For Development</p>
                     <div className="tasks-list">
                         {notes != null && notes.map(note => (
-                            <React.Fragment>
-                            {(note.status === "selected-for-development") &&
-                                <Note clickHandler={this.clickHandler} heading={note.heading} description={note.description} />}
+                            <React.Fragment key={note.id}>
+                            {(note.status === "selected") &&
+                                <div id={`outerdiv-${note.id}`} draggable="true" onDragStart={this.onDragStart} onDragEnd={this.onDragEnd}>
+                                    <Note clickHandler={this.clickHandler} note={note} />
+                                </div>}
                             </React.Fragment>
                         ))}
                     </div>
@@ -25,10 +92,13 @@ class Notes extends React.Component{
                 <div className="tasks-inprogress tasks-col">
                     <p className="tasks-type">InProgress</p>
                     <div className="tasks-list">
+                        <div id="dropzone-inprogress" className="on-drop drop-from-selected" onDragOver={this.onDragOver} onDrop={this.onDrop}></div>
                         {notes != null && notes.map(note => (
-                            <React.Fragment>
+                            <React.Fragment key={note.id}>
                             {(note.status === "inprogress") &&
-                                <Note clickHandler={this.clickHandler} heading={note.heading} description={note.description} />}
+                                <div id={`outerdiv-${note.id}`} draggable="true" onDragStart={this.onDragStart} onDragEnd={this.onDragEnd}>
+                                    <Note clickHandler={this.clickHandler} note={note} />
+                                </div>}
                             </React.Fragment>
                         ))}
                     </div>
@@ -36,11 +106,11 @@ class Notes extends React.Component{
                 <div className="tasks-done tasks-col">
                     <p className="tasks-type">Done</p>
                     <div className="tasks-list">
+                        <div id="dropzone-done" className="on-drop drop-from-inprogress" onDragOver={this.onDragOver} onDrop={this.onDrop}></div>
                         {notes != null && notes.map(note => (
-                                <React.Fragment>
-                                {(note.status === "done") &&
-                                    <Note clickHandler={this.clickHandler} heading={note.heading} description={note.description} />}
-                                </React.Fragment>
+                            <React.Fragment key={note.id}>
+                                {(note.status === "done") && <Note clickHandler={this.clickHandler} note={note} />}
+                            </React.Fragment>
                         ))}
                     </div>
                 </div>
